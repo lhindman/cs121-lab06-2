c0a17ff9986ec9c9a63110dbd5e2c4df6c9477bf
BoiseState Student <student@u.boisestate.edu>
2022-01-06 17:31:54 -0700

    After Split

---
+++ MonitoringStation/MonitoringStationDriver.java
@@ -0,0 +1,7 @@
+public class MonitoringStationDriver 
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ RiverFlowStats/RiverFlowStats.java
@@ -0,0 +1,7 @@
+public class RiverFlowStats 
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
